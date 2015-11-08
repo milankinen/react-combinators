8f6d512186fababb8a39c768f4ab19fb31ebd1cd
Matti Lankinen <m.lankinen@iki.fi>
2015-11-08 03:20:32 +0200

    Remove noise comment

--- src/baconjs/createComponent.js
+++ src/baconjs/createComponent.js
@@ -2,32 +2,7 @@ import Bacon from "baconjs"
 import React from "react"
 import {keys, values, zip, zipObject} from "../util"
 
-/**
- * Wraps function (...args) => Observable into React.Component so that
- * properties that are passed from parent elements are transformed to EventStreams
- * (e.g. (prop1, prop2) => (Observable(prop1), Observable(prop2))
- *
- * Example:
- *
- *    const App = React.createClass({
- *      render() {
- *        const {width, height} = this.state   // change this somehow
- *        return (
- *          <div>
- *            Example
- *            <BMI width={width} height={height} />
- *          </div>
- *        )
- *      }
- *    })
- *
- *    const BMI = createComponent({width, height} => {
- *      const bmiP = Bacon.combineWith(weight, height, (w, h) => Math.round(w/(h * h * 0.0001)))
- *      return Bacon.combineTemplate(
- *        <div>BMI for {width} and {height} is {bmiP}</div>
- *      )
- *    })
- */
+
 export default function createComponent(renderFn) {
   return React.createClass({
     getInitialState() {
